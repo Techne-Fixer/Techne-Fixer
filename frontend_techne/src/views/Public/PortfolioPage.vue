687249359e70556b9661d6308b1d32861f3d92cb
Genshirog <50698104+Genshirog@users.noreply.github.com>
2026-01-26 01:49:38 +0800

    Develop (#14)
    * Features/service (#11)
    
    * Configure Vue publicPath for GitHub Pages
    
    * Configuring the ci.yaml
    
    * fixing a bit
    
    * updating the cd on the ci/cd pipeline
    
    * Fixing PublicLayout bug with NavBar.vue
    
    * Configure Vue publicPath for Vercel Deployment
    
    * Fix/ci workflow update (#5)
    
    * Reverting back to Github Pages
    
    * Fixing CI.yaml
    
    * Fix/ci workflow update (#6)
    
    * Reverting back to Github Pages
    
    * Fixing CI.yaml
    
    * Fixing the deployment.yaml
    
    * Fix/ci workflow update (#7)
    
    * Reverting back to Github Pages
    
    * Fixing CI.yaml
    
    * Fixing the deployment.yaml
    
    * adding environment on the workflow
    
    * Fix/ci workflow update (#8)
    
    * Reverting back to Github Pages
    
    * Fixing CI.yaml
    
    * Fixing the deployment.yaml
    
    * adding environment on the workflow
    
    * Configure Vite base path for github pages
    
    * Fix/ci workflow update (#9)
    
    * Reverting back to Github Pages
    
    * Fixing CI.yaml
    
    * Fixing the deployment.yaml
    
    * adding environment on the workflow
    
    * Configure Vite base path for github pages
    
    * Fixing looking for the cause of 404
    
    * basta
    
    * Fix/ci workflow update (#10)
    
    * Reverting back to Github Pages
    
    * Fixing CI.yaml
    
    * Fixing the deployment.yaml
    
    * adding environment on the workflow
    
    * Configure Vite base path for github pages
    
    * Fixing looking for the cause of 404
    
    * basta
    
    * PLEASE COMMIT
    
    * MVP services finished
    
    * Features/portfolio (#13)
    
    * Configure Vue publicPath for GitHub Pages
    
    * Configuring the ci.yaml
    
    * fixing a bit
    
    * updating the cd on the ci/cd pipeline
    
    * Fixing PublicLayout bug with NavBar.vue
    
    * Configure Vue publicPath for Vercel Deployment
    
    * Fix/ci workflow update (#5)
    
    * Reverting back to Github Pages
    
    * Fixing CI.yaml
    
    * Fix/ci workflow update (#6)
    
    * Reverting back to Github Pages
    
    * Fixing CI.yaml
    
    * Fixing the deployment.yaml
    
    * Fix/ci workflow update (#7)
    
    * Reverting back to Github Pages
    
    * Fixing CI.yaml
    
    * Fixing the deployment.yaml
    
    * adding environment on the workflow
    
    * Fix/ci workflow update (#8)
    
    * Reverting back to Github Pages
    
    * Fixing CI.yaml
    
    * Fixing the deployment.yaml
    
    * adding environment on the workflow
    
    * Configure Vite base path for github pages
    
    * Fix/ci workflow update (#9)
    
    * Reverting back to Github Pages
    
    * Fixing CI.yaml
    
    * Fixing the deployment.yaml
    
    * adding environment on the workflow
    
    * Configure Vite base path for github pages
    
    * Fixing looking for the cause of 404
    
    * basta
    
    * Fix/ci workflow update (#10)
    
    * Reverting back to Github Pages
    
    * Fixing CI.yaml
    
    * Fixing the deployment.yaml
    
    * adding environment on the workflow
    
    * Configure Vite base path for github pages
    
    * Fixing looking for the cause of 404
    
    * basta
    
    * PLEASE COMMIT
    
    * MVP services finished
    
    * MVP portfolio

--- frontend_techne/src/views/Public/PortfolioPage.vue
+++ frontend_techne/src/views/Public/PortfolioPage.vue
@@ -1,12 +1,14 @@
 <template>
   <div class="portfolio-page">
     <HeroSection />
+    <PortfolioSection />
     <ContactUsSection/>
   </div>
 </template>
 
 <script setup>
 import HeroSection from '@/components/public/HeroSection.vue'; // <--- Import the new component
+import PortfolioSection from '@/components/public/portfolio/PortfolioSection.vue'; // <--- Import the new component
 import ContactUsSection from '@/components/public/ContactUsSection.vue'; // <--- Import the new component
 </script>
 
