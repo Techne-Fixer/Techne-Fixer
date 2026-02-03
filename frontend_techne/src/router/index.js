e3917ebee3c3192e0eeff9d7f236d82e9204e1b9
Your Name <your-email@example.com>
2026-02-03 20:11:11 +0800

    About us, still working on feedbacks

--- frontend_techne/src/router/index.js
+++ frontend_techne/src/router/index.js
@@ -1,50 +1,50 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHistory } from "vue-router";
 
 // Import your page components
-import Home from '@/views/Public/HomePage.vue';
-import Services from '@/views/Public/ServicesPage.vue';
-import Portfolio from '@/views/Public/PortfolioPage.vue';
-// import About from '@/views/About.vue';
+import Home from "@/views/Public/HomePage.vue";
+import Services from "@/views/Public/ServicesPage.vue";
+import Portfolio from "@/views/Public/PortfolioPage.vue";
+import About from "@/components/public/AboutUs.vue";
 // import Contact from '@/views/Contact.vue';
 // import Login from '@/views/Login.vue';
 // import Register from '@/views/Register.vue';
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    path: "/",
+    name: "Home",
+    component: Home,
   },
   {
-    path: '/services',
-    name: 'Services',
-    component: Services
+    path: "/services",
+    name: "Services",
+    component: Services,
   },
   {
-    path: '/portfolio',
-    name: 'Portfolio',
-    component: Portfolio
+    path: "/portfolio",
+    name: "Portfolio",
+    component: Portfolio,
   },
-//   {
-//     path: '/about',
-//     name: 'About',
-//     component: About
-//   },
-//   {
-//     path: '/contact',
-//     name: 'Contact',
-//     component: Contact
-//   },
-//   {
-//     path: '/login',
-//     name: 'Login',
-//     component: Login
-//   },
-//   {
-//     path: '/register',
-//     name: 'Register',
-//     component: Register
-//   }
+  {
+    path: "/about",
+    name: "About",
+    component: About,
+  },
+  //   {
+  //     path: '/contact',
+  //     name: 'Contact',
+  //     component: Contact
+  //   },
+  //   {
+  //     path: '/login',
+  //     name: 'Login',
+  //     component: Login
+  //   },
+  //   {
+  //     path: '/register',
+  //     name: 'Register',
+  //     component: Register
+  //   }
 ];
 
 const router = createRouter({
@@ -56,7 +56,7 @@ const router = createRouter({
     } else {
       return { top: 0 };
     }
-  }
+  },
 });
 
 export default router;
